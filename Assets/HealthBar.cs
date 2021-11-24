aa5956e4610e7cc5aea5a5e324abe0906dbc8c6a
kzyong17 <kzyong4@gmail.com>
2021-11-24 23:07:29 +0800

    Added Font + HP Bar
    New Assets

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBar : MonoBehaviour
+{
+    private Image HealthBars;
+    public float CurrentHealth;
+    private float MaxHealth = 100f;
+    PlayerStats Player;
+
+    public TextMeshProUGUI PlayerHPNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBars = GetComponent<Image>();
+        Player = FindObjectOfType<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentHealth = Player.Health;
+        HealthBars.fillAmount = CurrentHealth / MaxHealth;
+        PlayerHPNum.text = CurrentHealth.ToString("F0") + "/" + MaxHealth.ToString("F0");
+    }
+}
